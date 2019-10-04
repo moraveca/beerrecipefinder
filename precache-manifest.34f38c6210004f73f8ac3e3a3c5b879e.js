f56b0779df9bce25800d1e5497c8602e9996c924
arm <moraveca@gmail.com>
2019-10-03 22:09:14 -0500

    Updates

---
+++ precache-manifest.34f38c6210004f73f8ac3e3a3c5b879e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddd941a7d953c261e7fec87cfdac3e6",
+    "url": "/beerrecipefinder/index.html"
+  },
+  {
+    "revision": "9dde32d8b8d5cf6ae2ea",
+    "url": "/beerrecipefinder/static/css/main.e4bc7f83.chunk.css"
+  },
+  {
+    "revision": "ecf34c38bdb82c0a129a",
+    "url": "/beerrecipefinder/static/js/2.91baa7d1.chunk.js"
+  },
+  {
+    "revision": "9dde32d8b8d5cf6ae2ea",
+    "url": "/beerrecipefinder/static/js/main.f302398d.chunk.js"
+  },
+  {
+    "revision": "3b6fd2435262d9c45ff7",
+    "url": "/beerrecipefinder/static/js/runtime-main.73ddeacb.js"
+  }
+]);
